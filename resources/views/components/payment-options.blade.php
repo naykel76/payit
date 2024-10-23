d58fe6f4c7778fb3f19521f4d21bb934ba26f6d5
Nathan Watts <nathan@naykel.com.au>
2024-10-23 10:49:10 +1000

    wip

--- resources/views/components/payment-options.blade.php
+++ resources/views/components/payment-options.blade.php
@@ -1,54 +1,46 @@
 <div class="bx">
 
-    {{-- @if(session('cart')->total)
+    {{-- @if (session('cart')->total)
         <p class="txt-lg"><strong>Total: </strong>${{ number_format(session('cart')->total, 2) }}</p>
     @else
         <p class="txt-lg"><strong>Total: </strong>${{ session('cart.total') }}</p>
     @endif --}}
 
     <form id="paymentForm" action="{{ route('payment.pay') }}" method="POST">
-
         @csrf
-
         <div x-data="{}">
-
-            @foreach($paymentPlatforms as $paymentPlatform)
-
+            @foreach ($paymentPlatforms as $paymentPlatform)
                 <div x-data="{
-                        id: {{ $paymentPlatform->id }},
-                        get expanded() { return this.active === this.id },
-                        set expanded(value) { this.active = value ? this.id : null },
-                        }" role="region">
-
+                    id: {{ $paymentPlatform->id }},
+                    get expanded() { return this.active === this.id },
+                    set expanded(value) { this.active = value ? this.id : null },
+                }" role="region">
                     <div class="bdr pxy">
-
                         <div class="flex space-between">
-
                             <label for="{{ $paymentPlatform->id }}">
-
                                 <input @click="expanded = !expanded" name="payment_platform" id="{{ $paymentPlatform->id }}" value="{{ $paymentPlatform->id }}" type="radio" aria-controls="collapse">
-                                <span> {{ $paymentPlatform->method }}</span>
-
+                                <span> {{ $paymentPlatform->label }}</span>
                             </label>
-
                         </div>
 
                         <div x-show="expanded" x-collapse>
-
                             @php
                                 $providerComponent = str_replace(' ', '-', strtolower($paymentPlatform->method));
                             @endphp
-
                             @includeIf("payit::components.$providerComponent-collapse")
-
                         </div>
 
+                        {{ $paymentPlatform->method ?? 'none selected' }}
+
+                        <!-- display the selected payment component -->
+                        <div x-show="expanded" x-collapse class="pxy">
+                            <?php $providerComponent = str_replace(' ', '-', strtolower($paymentPlatform['method'])); ?>
+                            @includeIf("payit::components.payment-methods.$providerComponent")
+                        </div>
                     </div>
 
                 </div>
-
             @endforeach
-
         </div>
 
         <div class="frm-row mt">
@@ -57,10 +49,10 @@
             </label>
         </div>
 
-        @if(isset($errors) && $errors->any())
+        @if (isset($errors) && $errors->any())
             <div class="bx danger mt" role="alert">
                 <ul>
-                    @foreach($errors->all() as $error)
+                    @foreach ($errors->all() as $error)
                         <li>{{ $error }}</li>
                     @endforeach
                 </ul>
---
+++ src/Services/PaymentService.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Naykel\Payit\Services;
+
+use Naykel\Payit\Traits\ConsumesExternalServices;
+
+abstract class PaymentService
+{
+    use ConsumesExternalServices;
+
+    protected string $baseUri;
+
+    protected string $key;
+
+    protected string $secret;
+
+    /**
+     * PaymentService constructor.
+     *
+     * Initializes the payment service with the configuration for the specific service.
+     *
+     * @param  string  $serviceConfigKey  The key for the specific service in the configuration.
+     */
+    public function __construct(string $serviceConfigKey)
+    {
+        $configKeys = $this->mapApiConfigKeys();
+        $config = config("payit.{$serviceConfigKey}");
+
+        $this->baseUri = $config[$configKeys['base_uri']];
+        $this->key = $config[$configKeys['key']];
+        $this->secret = $config[$configKeys['secret']];
+    }
+
+    public function resolveAuthorization(&$queryParams, &$formParams, &$headers)
+    {
+        $headers['Authorization'] = $this->resolveAccessToken();
+    }
+
+    public function decodeResponse($response)
+    {
+        return json_decode($response);
+    }
+
+    /**
+     * Map the keys for a specific service.
+     *
+     * This method should be implemented by each specific service class to
+     * return the correct map of configuration keys.
+     *
+     * Solves the problem of having different keys for different services.
+     *
+     * @return array The map of configuration keys for the specific service.
+     */
+    abstract protected function mapApiConfigKeys(): array;
+
+    abstract protected function resolveAccessToken();
+
+    abstract public function handlePayment($total, $request);
+
+    abstract public function handleApproval();
+}
